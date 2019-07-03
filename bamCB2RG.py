91385943a2843808bca41e0b9de36282b26a3b00
Kevin Murray <kdmfoss@gmail.com>
2019-07-03 14:41:07 +1000

    Add progress logging

--- bamCB2RG.py
+++ bamCB2RG.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pysam
 from collections import Counter
+from sys import stdin, stdout, stderr
 import argparse
 
 
@@ -15,25 +16,33 @@ def main():
 
     newrgs = {}
     rgsmlist = set()
+    print("Reading bam to generate new header", file=stderr)
     with pysam.AlignmentFile(args.input_bam) as bam:
         oldrgs = {d["ID"]: d for d in bam.header["RG"]}
-        for aln in bam:
+        for i, aln in enumerate(bam):
             rgdata = oldrgs[aln.get_tag('RG')].copy()
             rgid = aln.get_tag('RG') + "_" + aln.get_tag('CB')
             rgsm = rgdata["SM"] + "_" + aln.get_tag('CB')
             rgdata["ID"] = rgid
             rgdata["SM"] = rgsm
             newrgs[rgid] = rgdata
+            if i > 0 and i % 100000 == 0:
+                print("\t{:0.0f}k reads".format(i/1000), file=stderr)
         newheader = bam.header.to_dict()
+        print("\tDone making new header from", i, "reads", file=stderr)
 
     newheader["RG"] = [d for rid, d in sorted(newrgs.items())]
 
     with pysam.AlignmentFile(args.input_bam) as bam, \
         pysam.AlignmentFile(args.output_bam, "wb", header=newheader) as out:
-        for aln in bam:
+        print("Writing new bam file with corrected read tags", file=stderr)
+        for i, aln in enumerate(bam):
             rgid = aln.get_tag('RG') + "_" + aln.get_tag('CB')
             aln.set_tag("RG", rgid)
             out.write(aln)
+            if i > 0 and i % 100000 == 0:
+                print("\t{:0.0f}k reads".format(i/1000), file=stderr)
+        print("\tDone making new bam! Total of", i, "reads", file=stderr)
 
 if __name__ == "__main__":
     main()
